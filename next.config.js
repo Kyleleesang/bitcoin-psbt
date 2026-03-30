7deb1c9ba5314eed656e286137d1d428ae6741df
Kyle <Kyleleesang117@gmail.com>
2026-03-30 00:38:05 -0400

    deploy using github pages

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@ const webpack = require('webpack');
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  basePath: process.env.NEXT_PUBLIC_BASE_PATH || '',
   images: { unoptimized: true },
   webpack: (config, { isServer }) => {
     if (!isServer) {
